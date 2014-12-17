566ad98c2219efd31b98de52039af29371b7ddfd
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-12-17 01:14:43 +0000

    Calculate the grid, better way.

--- src/grid.c
+++ src/grid.c
@@ -12,15 +12,10 @@ void print_grid(WINDOW *win)
     int32_t y_grid_step = win_set.maxGridHeight / g.y_grid;
     int32_t x_grid_step = win_set.maxGridWidth / g.x_grid;
 
-    for(int32_t y=0;y<win_set.maxGridHeight;y++)
-        for(int32_t x=0;x<win_set.maxGridWidth;x++)
-        {
-            if (!(y % y_grid_step ||
-                  x % x_grid_step
-                  ))
-                color_str(win, y, x, 0, 0, "O");
+    for(int32_t y=0;y<win_set.maxGridHeight;y+=y_grid_step)
+        for(int32_t x=0;x<win_set.maxGridWidth;x+=x_grid_step)
+            color_str(win, y, x, 0, 0, "O");
 
-        }
 
     wnoutrefresh(win);
 }
