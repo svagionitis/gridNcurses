e6f8e0737b36c6e9c40e5a9edca74a3df27a6c31
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-12-23 00:01:57 +0000

    Correcting the output on some values, like 8x8. Check if the grid is larger than maximum height or width.

--- src/grid.c
+++ src/grid.c
@@ -9,11 +9,22 @@ void print_grid(WINDOW *win)
 {
     getmaxyx(win, win_set.maxGridHeight, win_set.maxGridWidth);
 
+    // If the grid is larger than the maximum height
+    // or width, then set it to max height or width
+    if (g.y_grid > win_set.maxGridHeight)
+        g.y_grid = win_set.maxGridHeight;
+
+    if (g.x_grid > win_set.maxGridWidth)
+        g.x_grid = win_set.maxGridWidth;
+
     int32_t y_grid_step = win_set.maxGridHeight / g.y_grid;
     int32_t x_grid_step = win_set.maxGridWidth / g.x_grid;
 
-    for(int32_t y=0;y<win_set.maxGridHeight;y+=y_grid_step)
-        for(int32_t x=0;x<win_set.maxGridWidth;x+=x_grid_step)
+    int32_t y_max_height = y_grid_step * g.y_grid;
+    int32_t x_max_height = x_grid_step * g.x_grid;
+
+    for(int32_t y=0;y<=y_max_height;y+=y_grid_step)
+        for(int32_t x=0;x<=x_max_height;x+=x_grid_step)
             color_str(win, y, x, 0, 0, "O");
 
 
