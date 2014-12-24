85f13da3c69ef2109ed4b03cb7644ef1c05203b2
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-12-24 00:04:57 +0000

    Print the grid as more central as possible.

--- src/grid.c
+++ src/grid.c
@@ -20,19 +20,22 @@ void print_grid(WINDOW *win)
     int32_t y_grid_step = win_set.maxGridHeight / g.y_grid;
     int32_t x_grid_step = win_set.maxGridWidth / g.x_grid;
 
-    int32_t y_max_height = win_set.maxGridHeight - (win_set.maxGridHeight % g.y_grid);
-    int32_t x_max_width = win_set.maxGridWidth - (win_set.maxGridWidth % g.x_grid);
+    int32_t y_remaining = win_set.maxGridHeight % g.y_grid;
+    int32_t x_remaining = win_set.maxGridWidth % g.x_grid;
+
+    int32_t y_max_height = win_set.maxGridHeight - y_remaining;
+    int32_t x_max_width = win_set.maxGridWidth - x_remaining;
 
     for(int32_t y=0;y<=y_max_height;y++)
         for(int32_t x=0;x<=x_max_width;x++)
             if (!(y % y_grid_step) && !(x % x_grid_step))
-                color_str(win, y, x, 0, 0, "+");
+                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "+");
             else if (!(y % y_grid_step))
-                color_str(win, y, x, 0, 0, "-");
+                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "-");
             else if (!(x % x_grid_step))
-                color_str(win, y, x, 0, 0, "|");
+                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "|");
             else if ((y % y_grid_step) || (x % x_grid_step))
-                color_str(win, y, x, 0, 0, "X");
+                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "X");
 
 
     wnoutrefresh(win);
