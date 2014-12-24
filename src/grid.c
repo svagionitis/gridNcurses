101d635517cf7a172e2e476e284adf49b588c9f7
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-12-24 00:20:03 +0000

    Refactor the centering of grid on screen.

--- src/grid.c
+++ src/grid.c
@@ -23,19 +23,29 @@ void print_grid(WINDOW *win)
     int32_t y_remaining = win_set.maxGridHeight % g.y_grid;
     int32_t x_remaining = win_set.maxGridWidth % g.x_grid;
 
-    int32_t y_max_height = win_set.maxGridHeight - y_remaining;
-    int32_t x_max_width = win_set.maxGridWidth - x_remaining;
+    int32_t new_max_height = win_set.maxGridHeight - y_remaining;
+    int32_t new_max_width = win_set.maxGridWidth - x_remaining;
 
-    for(int32_t y=0;y<=y_max_height;y++)
-        for(int32_t x=0;x<=x_max_width;x++)
+    for(int32_t y = 0;y <= new_max_height;y++)
+    {
+        // Adding the half of the remaining in order
+        // to put the grid more center on screen.
+        int32_t new_y = y + (y_remaining / 2);
+
+        for(int32_t x = 0;x <= new_max_width;x++)
+        {
+            int32_t new_x = x + (x_remaining / 2);
+
             if (!(y % y_grid_step) && !(x % x_grid_step))
-                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "+");
+                color_str(win, new_y, new_x, 0, 0, "+");
             else if (!(y % y_grid_step))
-                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "-");
+                color_str(win, new_y, new_x, 0, 0, "-");
             else if (!(x % x_grid_step))
-                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "|");
+                color_str(win, new_y, new_x, 0, 0, "|");
             else if ((y % y_grid_step) || (x % x_grid_step))
-                color_str(win, y+(y_remaining/2), x+(x_remaining/2), 0, 0, "X");
+                color_str(win, new_y, new_x, 0, 0, "X");
+        }
+    }
 
 
     wnoutrefresh(win);
