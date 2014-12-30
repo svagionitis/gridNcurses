f00035de368a5a8ed8bcb7c9eec06bedf1c5dfe3
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2014-12-30 02:32:34 +0000

    Add MIN and MAX macros.

--- include/utils.h
+++ include/utils.h
@@ -4,6 +4,9 @@
 #include <curses.h>
 #include <stdint.h>
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 void color_str(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *);
 void init_seed_srand(void);
 
