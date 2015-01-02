c1322cf6759107553377e8b36d1478ddb31072b4
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2015-01-02 23:49:30 +0000

    Refactor how to print in each cell.

--- src/grid.c
+++ src/grid.c
@@ -93,11 +93,10 @@ void generate_grid(WINDOW *win, int32_t grid_win_height, int32_t grid_win_width,
                 // Print in each grid the number of the grid "linearly"
                 // It doesn't work well with two digits and more numbers.
                 // It prints only the first digit.
-                //print_num_in_all_grid(((y / y_grid_step) * g.y_grid) + (x / x_grid_step), print_char);
-                print_num_in_all_grid(win, new_x, new_y, ((y / y_grid_step) * g.y_grid) + (x / x_grid_step), color_str);
-                //print_num_in_grid(2, 3, x, y, x_grid_step, y_grid_step, 7, print_char);
-                //print_x_in_grid(1, 1, x, y, x_grid_step, y_grid_step, print_char);
-                //print_o_in_grid(1, 2, x, y, x_grid_step, y_grid_step, print_char);
+                //print_num_in_all_grid(win, new_x, new_y, ((y / y_grid_step) * g.y_grid) + (x / x_grid_step), color_str);
+                print_num_in_grid(win, 2, 3, x, y, x_grid_step, y_grid_step, new_x, new_y, 7, color_str);
+                print_x_in_grid(win, 1, 1, x, y, x_grid_step, y_grid_step, new_x, new_y, color_str);
+                print_o_in_grid(win, 1, 2, x, y, x_grid_step, y_grid_step, new_x, new_y, color_str);
             }
 
             (*print_scr)(win, new_y, new_x, 0, 0, print_char);
@@ -105,28 +104,35 @@ void generate_grid(WINDOW *win, int32_t grid_win_height, int32_t grid_win_width,
     }
 }
 
-void print_num_in_all_grid(WINDOW *win, int32_t x, int32_t y, int32_t num, void (*print_scr)(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *))
+void print_num_in_all_grid(WINDOW *win, int32_t print_x, int32_t print_y, int32_t num, void (*print_scr)(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *))
 {
     char str_out[10];
     memset(str_out, 0, sizeof(str_out));
 
     sprintf(str_out, "%d", num);
 
-    (*print_scr)(win, y, x, 0, 0, str_out);
+    (*print_scr)(win, print_y, print_x, 0, 0, str_out);
 }
 
-void print_num_in_grid(int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32_t x_step, int32_t y_step, int32_t num, char *str_out)
+void print_num_in_grid(WINDOW *win, int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32_t x_step, int32_t y_step, int32_t print_x, int32_t print_y, int32_t num, void (*print_scr)(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *))
 {
+    char str_out[10];
+    memset(str_out, 0, sizeof(str_out));
+
     if (grid_x == (x / x_step) &&
         grid_y == (y / y_step))
+    {
         sprintf(str_out, "%d", num);
-    else
-        sprintf(str_out, " ");
 
+        (*print_scr)(win, print_y, print_x, 0, 0, str_out);
+    }
 }
 
-void print_x_in_grid(int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32_t x_step, int32_t y_step, char *str_out)
+void print_x_in_grid(WINDOW *win, int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32_t x_step, int32_t y_step, int32_t print_x, int32_t print_y, void (*print_scr)(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *))
 {
+    char str_out[10];
+    memset(str_out, 0, sizeof(str_out));
+
     if (grid_x == (x / x_step) &&
         grid_y == (y / y_step))
     {
@@ -137,7 +143,6 @@ void print_x_in_grid(int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32
         int32_t slope_right_diag = cell_x / (y_step - cell_y);
         int32_t slope_step = x_step / y_step;
 
-
         // \ diagonal
         if (slope_left_diag == slope_step && (cell_x % cell_y) == cell_y - 1)
             sprintf(str_out, "\\");
@@ -146,14 +151,16 @@ void print_x_in_grid(int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32
             sprintf(str_out, "/");
         else
             sprintf(str_out, " ");
-    }
-    else
-        sprintf(str_out, " ");
 
+        (*print_scr)(win, print_y, print_x, 0, 0, str_out);
+    }
 }
 
-void print_o_in_grid(int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32_t x_step, int32_t y_step, char *str_out)
+void print_o_in_grid(WINDOW *win, int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32_t x_step, int32_t y_step, int32_t print_x, int32_t print_y, void (*print_scr)(WINDOW *, uint32_t, uint32_t, int16_t, int16_t, const char *))
 {
+    char str_out[10];
+    memset(str_out, 0, sizeof(str_out));
+
     if (grid_x == (x / x_step) &&
         grid_y == (y / y_step))
     {
@@ -167,9 +174,7 @@ void print_o_in_grid(int32_t grid_x, int32_t grid_y, int32_t x, int32_t y, int32
         else
             sprintf(str_out, " ");
 
+        (*print_scr)(win, print_y, print_x, 0, 0, str_out);
     }
-    else
-        sprintf(str_out, " ");
-
 }
 
